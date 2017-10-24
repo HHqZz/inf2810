4d3f424ee443d721441835fffc6c5c75b2ee1b89
HHqZz <cbouis@outlook.fr>
2017-10-24 02:06:25 -0400

    hasse manque transitivité et print

--- src/GraphTools/Hasse.java
+++ src/GraphTools/Hasse.java
@@ -1,24 +1,36 @@
 package GraphTools ;
 
+import java.io.IOException;
 
 public class Hasse{
 	
 	//constructeur
 	//todo
+	public static Graph creerGrapheOriente() throws IOException {
+        Graph graphRecette = new Graph();
+        graphRecette.fillGraph("manger.txt",false,true );	// On cree le graph oriente
+        return graphRecette;
+    }
 	public Hasse(){
 		
 	}
 	
 	//todo
-	public void retirerTransitivite(){	
-		
-		
-	} 
+	public void retirerTransitivite(Graph graph){	// Pour chaque Noeud, ses 'to' doivent être differents des "to" de ses "to"  CA SE COMPREND SUR PAPIER    to = destination d'un arc
+		for(Node nodes: graph.getNodes()){ // pour chaque noeud
+
+		}
+
 	
 	
 	//todo
-	public void retirerReflexivite(){		// Parcourir ARC : si from == to alors retirer arc
-		
+	public void retirerReflexivite(Graph graph){		// Parcourir ARC : si from == to alors retirer arc
+		for (Edge edge: graph.getEdges()		// on épluche les edges du graphe
+			 ) {
+			if (edge.getFrom() == edge.getTo()){ // si les noeude to et from sont les mêmes
+				graph.removeEdge(edge); //			on l'enlève du graphe
+			}
+		}
 	}
 	
 	//todo
@@ -27,9 +39,8 @@ public class Hasse{
 		retirerTransitivite();
 	}
 	
-	//todo
-	public void printHass(){
-		
-	}
+	//va bene
+
+
 	
 }
--- src/TestMain.java
+++ src/TestMain.java
@@ -1,20 +1,22 @@
 import GraphTools.FonctionDrones;
 import GraphTools.Graph;
-
+import GraphTools.Hasse;
 import java.io.IOException;
 
 public class TestMain {
     public static void main(String[] args) throws IOException {
-        Graph graphDrone = new Graph();
-        graphDrone.fillGraph("arrondissements.txt",true,false );
+        Graph recetteGraph = Hasse.creerGrapheOriente();
+        recetteGraph.printGraphOriente();
+//        Graph graphDrone = new Graph();
+//        graphDrone.fillGraph("arrondissements.txt",true,false );
 //        graphDrone.lireGraph();
-        FonctionDrones.plusCourtChemin(graphDrone,"1","17",3);
-        FonctionDrones.plusCourtChemin(graphDrone,"1","17",2);
-        FonctionDrones.plusCourtChemin(graphDrone,"1","17",1);
-        FonctionDrones.plusCourtChemin(graphDrone,"1","7",3);
-        FonctionDrones.plusCourtChemin(graphDrone,"1","7",2);
-        FonctionDrones.plusCourtChemin(graphDrone,"1","7",1);
-        FonctionDrones.plusCourtChemin(graphDrone,"1","13",1);
-        FonctionDrones.plusCourtChemin(graphDrone,"8","15",1);
+//        FonctionDrones.plusCourtChemin(graphDrone,"1","17",3);
+//        FonctionDrones.plusCourtChemin(graphDrone,"1","17",2);
+//        FonctionDrones.plusCourtChemin(graphDrone,"1","17",1);
+//        FonctionDrones.plusCourtChemin(graphDrone,"1","7",3);
+//        FonctionDrones.plusCourtChemin(graphDrone,"1","7",2);
+//        FonctionDrones.plusCourtChemin(graphDrone,"1","7",1);
+//        FonctionDrones.plusCourtChemin(graphDrone,"1","13",1);
+//        FonctionDrones.plusCourtChemin(graphDrone,"8","15",1);
     }
 }
