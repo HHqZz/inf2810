63cd1e7a4e9aa8846f4fe1ddbaff11aac4613b71
HHqZz <cbouis@outlook.fr>
2017-11-15 17:45:58 -0500

    station request et main

---
+++ TP2/TPautomates/station.py
@@ -0,0 +1,17 @@
+import request
+
+
+
+class station :
+
+    def __init__(self):
+        self
+
+    nomStation = ''
+    listeRequetes = []
+
+    def setNom(nom):
+        nomStation = nom
+
+    def setListeRequete :
+
